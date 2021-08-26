91409dab02f1dea8c03985f426295dde99a28ca0
Elton Freires <elton.freires@gmail.com>
2021-08-26 16:09:29 -0300

    pag09 concluída

--- JSProgDoZero.js
+++ JSProgDoZero.js
@@ -353,11 +353,22 @@ console.log(somarNumeros([15, 15, 15, 15])); */
 //                  {nome: "Cinema", categoria: "Entretenimento", preco: 150} ]) // retornará 239.99
 // despesasTotais([ {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
 //                  {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90} ]) // retornará 34599.89
-function despesasTotais(el){
-
+// function despesasTotais(produtos) {
+//     let soma = 0;
+//     for (let obj of produtos) {
+//         soma += obj.preco;
+//     }    
+//     return soma;
+// }
+// function despesasTotais(produtos){
+//     return produtos.map(item => item.preco).reduce((acumulador, value) => acumulador += value,0);
+// }
+function despesasTotais(produtos){
+    return produtos.reduce((acumulador, value) => acumulador += value.preco,0);
 }
-console.log(despesasTotais([ {nome: "Jornal online", categoria: "Informação", preco: 89.99}, {nome: "Cinema", categoria: "Entretenimento", preco: 150} ]));
-console.log(despesasTotais([ {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99}, {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90} ]));
+console.log(despesasTotais([{ nome: "Jornal online", categoria: "Informação", preco: 89.99 }, { nome: "Cinema", categoria: "Entretenimento", preco: 150 }]));
+console.log(despesasTotais([{ nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99 }, { nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90 }]));
+
 
 
 /*** Pág 10 ***/
@@ -375,37 +386,7 @@ console.log(despesasTotais([ {nome: "Galaxy S20", categoria: "Eletrônicos", pre
 
 
 // **************************************************************************************************
-// Você está trabalhando numa aplicação pessoal de controle de despesas. Na tela principal dessa aplicação, é
-// possível adicionar produtos ou serviços, informando nome, categoria e preço. Uma funcionalidade que você está
-// desenvolvendo no momento é a de somar o total das despesas.
-// Crie uma função que receba um array de produtos e retorne o total das despesas.
-// Exemplos:
-// despesasTotais([
-// {nome: "Jornal online", categoria: "Informação", preco: 89.99},
-// {nome: "Cinema", categoria: "Entretenimento", preco: 150}
-// ]) // retornará 239.99
-// despesasTotais([
-// {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
-// {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
-// ]) // retornará 34599.89
-// Resolução 1
-// function despesasTotais(itens) {
-// var total = 0
-// for (let item of itens)
-// total += item.preco
-// return total
-// }
-// Resolução 2
-// function despesasTotais(itens) {
-// return itens
-// .map(item => item.preco)
-// .reduce((acumulador, valorAtual) => acumulador + valorAtual)
-// }
-// Resolução 3
-// function despesasTotais(itens) {
-// return itens.reduce((acumulador, valorAtual) => acumulador + valorAtual.preco, 0)
-// }
-// Resoluções - Exercícios - Curso Fundamentos de Programação 10
+
 // Numa aplicação Web de investimento financeiro da qual você faz parte da equipe de desenvolvimento, pretendese adicionar a funcionalidade de calcular a média de um conjunto de números informados pelo usuário.
 // Com o intuito de realizar esse cálculo, crie uma função que receba um array com uma quantidade indeterminada
 // de números e retorne a média simples desses números.
