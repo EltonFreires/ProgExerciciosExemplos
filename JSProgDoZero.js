e603af9fa5bcd03d6d1cfdc66cddc253ef2a4551
Elton Freires <elton.freires@gmail.com>
2021-08-31 13:51:32 -0300

    pag12 concluida

--- JSProgDoZero.js
+++ JSProgDoZero.js
@@ -49,9 +49,7 @@
 //         case 1: return "Março";
 //         case 4: return "Abril";
 //         default:
-//             break;
-//     }
-// }
+//             break; } }
 // function receberNomeDoMes(mes) {
 //     const mesesAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'] ;
 //     return mesesAno[--mes];
@@ -62,13 +60,9 @@
 
 // Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo. Deverá diferenciar números de strings.
 // function maiorOuIgual(valor1, valor2){
-//     if(typeof valor1 === "string" || typeof valor2 === "string")
-//         return false;
-//     else if (valor2 > valor1) {
-//         return false;
-//     } 
-//     return true
-// }
+//     if(typeof valor1 === "string" || typeof valor2 === "string")  return false;
+//     else if (valor2 > valor1) { return false;  } 
+//     return true }
 // console.log(maiorOuIgual(0,0)); // retornará true
 // console.log(maiorOuIgual(5,1)); // retornará true
 // console.log(maiorOuIgual(2,5));
@@ -82,178 +76,120 @@
 // Escreva uma função que receba um valor booleano ou numérico. Se o parâmetro fornecido for booleano, o retorno da função deverá ser o inverso. Por exemplo, se a entrada for false, retornará true. 
 // Se o parâmetro for numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será -1. 
 // Se o parâmetro de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do tipo
-// Exemplos:
-// inverso(true) // retornará false
-// inverso("6") // retornará "booleano ou número esperados, mas o parâmetro é do tipo string"
-// inverso(-2000) // retornará 2000
-// inverso("programação") // retornará "booleano ou números, mas o parâmetro é do tipo string"
-/* function inverso(valor){
-    if (typeof valor === "boolean") {
-        return !valor;
-    }
-    else if (typeof valor === "number" ) {
-        return valor*(-1);
-    }
-    return `Booleano ou número esperados, mas o parâmetro é do tipo ${typeof valor}`;
-}
-console.log(inverso(true));
-console.log(inverso("6"));
-console.log(inverso(-2000));
-console.log(inverso("programação")); */
-
+// function inverso(valor){
+//     if (typeof valor === "boolean") { return !valor; }
+//     else if (typeof valor === "number" ) { return valor*(-1); }
+//     return `Booleano ou número esperados, mas o parâmetro é do tipo ${typeof valor}`;
+// }
+// console.log(inverso(true)); // retornará false
+// console.log(inverso("6")); // retornará "booleano ou número esperados, mas o parâmetro é do tipo string"
+// console.log(inverso(-2000)); // retornará 2000
+// console.log(inverso("programação"));  // retornará "booleano ou números, mas o parâmetro é do tipo string"
 
 
 // Crie uma função que receba quatro números como parâmetro (minimo, maximo, numero, inclusivo) e retorne se o parâmetro numero (o primeiro) está entre minimo e maximo. 
 // Quando o parâmetro inclusivo for true, tenha "entre" como inlusivo, ou seja, considerando se numero é igual a minimo ou a maximo. 
 // Caso o parâmetro inclusivo não seja informado, seu valor padrão deverá ser false, portanto, a lógica será exclusiva, não considerando se numero é igual a minimo ou a maximo.
-// Exemplos:
-// estaEntre(10, 100, 50) // retornará true
-// estaEntre(16, 100, 160) // retornará false
-// estaEntre(3, 150, 3) // retornará false
-// estaEntre(3, 150, 3, true) // retornará true
-/* function estaEntre(minimo, maximo, numero, inclusivo){
-    if (numero > minimo && numero < maximo || (inclusivo && (numero === minimo || numero === maximo ))) {
-        return true;
-    }
-    return false;
-}
-console.log(estaEntre(10, 100, 50));
-console.log(estaEntre(16, 100, 160));
-console.log(estaEntre(3, 150, 3));
-console.log(estaEntre(3, 150, 3, true)); */
+// function estaEntre(minimo, maximo, numero, inclusivo){
+//     if (numero > minimo && numero < maximo || (inclusivo && (numero === minimo || numero === maximo ))) { return true; }
+//     return false;
+// }
+// console.log(estaEntre(10, 100, 50)); // retornará true
+// console.log(estaEntre(16, 100, 160)); // retornará false
+// console.log(estaEntre(3, 150, 3)); // retornará false
+// console.log(estaEntre(3, 150, 3, true)); // retornará true
 
 
 
 /*** Pág 04 ***/
 
-/* // Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a multiplicação deles. Porém, não utilize o operador de mutiplicação.
-// Exemplo:
-// multiplicar(5, 5) // retornará 25
-// multiplicar(0, 7) // retornará 0
-function multiplicar(num1, num2){
-    const maiorNum = Math.max(num1, num2);
-    const menorNum = Math.min(num1, num2);
-    let total = 0;
-    if (!(num1 === 0 || num2 === 0)) {
-        for (let i = 0; i < menorNum; i++) {
-            total += maiorNum;        
-        }
-    }    
-    return total;
-}
-console.log(multiplicar(4, 5));
-console.log(multiplicar(5, 3));
-console.log(multiplicar(0, 7)); */
+// Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a multiplicação deles. Porém, não utilize o operador de mutiplicação.
+// function multiplicar(num1, num2){
+//     const maiorNum = Math.max(num1, num2);
+//     const menorNum = Math.min(num1, num2);
+//     let total = 0;
+//     if (!(num1 === 0 || num2 === 0)) {
+//         for (let i = 0; i < menorNum; i++) { total += maiorNum; }
+//     }    
+//     return total;
+// }
+// console.log(multiplicar(4, 5)); // retornará 20
+// console.log(multiplicar(5, 3)); // retornará 15
+// console.log(multiplicar(0, 7)); // retornará 0
 
 
-
-/* // Escreva uma função que receba dois parâmetros. O primeiro parâmetro é o elemento que repetirá, enquanto que
+// Escreva uma função que receba dois parâmetros. O primeiro parâmetro é o elemento que repetirá, enquanto que
 // o segundo será o número de vezes que haverá repetição. Um array será retornado.
-// Exemplos:
-// repetir("código", 2) // retornará ["código", "código"]
-// repetir(7, 3) // retornará [7, 7, 7]
-function repetir(elemento, qtdRepeticao) {
-    let arrElementos = [];
-    for (let i = 0; i < qtdRepeticao; i++) {
-        arrElementos.push(elemento);
-    }
-    return arrElementos;
-}
-function repetir2(elemento, qtdRepeticao) {
-    return Array(qtdRepeticao).fill(elemento);
-}
-console.log(repetir("código", 2));
-console.log(repetir(7, 3));
-console.log(repetir2("código", 2));
-console.log(repetir2(7, 3)); */
+// function repetir(elemento, qtdRepeticao) {
+//     let arrElementos = [];
+//     for (let i = 0; i < qtdRepeticao; i++) { arrElementos.push(elemento); }
+//     return arrElementos;
+// }
+// function repetir(elemento, qtdRepeticao) {
+//     return Array(qtdRepeticao).fill(elemento);
+// }
+// console.log(repetir("código", 2)); // retornará ["código", "código"]
+// console.log(repetir(7, 3)); // retornará [7, 7, 7]
 
 
 
 /*** Pág 05 ***/
 
-/* // Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
+// Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
 // quantidade especificada no parâmetro.
-// Exemplos:
-// simboloMais(2) // retornará "++"
-// simboloMais(4) // retornará "++++"
-function simboloMais(qtdRepeticao){
-    let str = '';
-    for (let i = 0; i < qtdRepeticao; i++) {
-        str += "+"        
-    }
-    return str;
-}
-function simboloMais2(qtdRepeticao){
-    return Array(qtdRepeticao).fill("+").join("");
-}
-function simboloMais3(qtdRepeticao){
-    return "+".repeat(qtdRepeticao);
-}
-console.log(simboloMais(4));
-console.log(simboloMais2(4));
-console.log(simboloMais3(4)); */
+// function simboloMais(qtdRepeticao){
+//     let str = '';
+//     for (let i = 0; i < qtdRepeticao; i++) { str += "+" }
+//     return str;
+// }
+// function simboloMais2(qtdRepeticao){
+//     return Array(qtdRepeticao).fill("+").join("");
+// }
+// function simboloMais3(qtdRepeticao){
+//     return "+".repeat(qtdRepeticao);
+// }
+// console.log(simboloMais(4)); // retornará "++++"
 
 
-
-/* // Crie uma função que receba uma array e retorne o primeiro e o último elemento desse array como um novo array:
-// Exemplos:
-// receberPrimeiroEUltimoElemento([7,14,"olá"]) // retornará [7, "olá"]
-// receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]) // retornará [-100, 16]
+// Crie uma função que receba uma array e retorne o primeiro e o último elemento desse array como um novo array:
 // function receberPrimeiroEUltimoElemento(elementos){
 //     let a = [elementos[0], elementos[elementos.length - 1]];
 //     return a;
 // }
-function receberPrimeiroEUltimoElemento(elementos){
-    return [elementos.shift(), elementos.pop()];
-}
-console.log(receberPrimeiroEUltimoElemento([7,14,"olá"]));
-console.log(receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]));
-let arr = ["a","b","c"];
-console.log(arr);
-console.log(receberPrimeiroEUltimoElemento(arr));
-console.log(arr); */
+// function receberPrimeiroEUltimoElemento(elementos){
+//     return [elementos.shift(), elementos.pop()];
+// }
+// console.log(receberPrimeiroEUltimoElemento([7,14,"olá"])); // retornará [7, "olá"]
+// console.log(receberPrimeiroEUltimoElemento([-100, 16])); // retornará [-100, 16]
 
 
 
 /*** Pág 06 ***/
 
-/* // Você escreverá uma função que recebe um objeto como primeiro parâmetro e, como segundo parâmetro, o nome
+// Você escreverá uma função que recebe um objeto como primeiro parâmetro e, como segundo parâmetro, o nome
 // de uma propriedade contida nesse objeto. Em seguida, retorne uma cópia desse objeto sem a propriedade especificada no segundo parâmetro.
-// Exemplos:
-// removerPropriedade({a: 1, b: 2}, "a") // retornará {b: 2}
-// removerPropriedade({id: 20, nome: "caneta", descricao: "Não preenchido"}, "descricao") // retornará {id: 20, nome: "caneta"}
-// A fim de testar se o objeto retornado não é o mesmo que foi passado como parâmetro para a função
-// removerPropriedade, você poderá usar a função Object.is(), por exemplo:
-// Object.is(removerPropriedade(objeto, "descricao"), objeto) // Retornará false se o objeto não for o mesmo.
-function removerPropriedade(obj, parametroDeletar){
-    console.log(obj);
-    delete obj[parametroDeletar];    
-    return obj;
-}
-console.log(removerPropriedade({a: 1, b: 2}, "a"));
-console.log(removerPropriedade({id: 20, nome: "caneta", descricao: "Não preenchido"}, "descricao")); */
-
+// function removerPropriedade(obj, parametroDeletar){
+//     console.log(obj);
+//     delete obj[parametroDeletar];    
+//     return obj;
+// }
+// console.log(removerPropriedade({a: 1, b: 2}, "a")); // retornará {b: 2}
+// console.log(removerPropriedade({id: 20, nome: "caneta", descricao: "Não preenchido"}, "descricao")); // Retornará { id: 20, nome: 'caneta' }
 
 
 // Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no array recebido como parâmetro.
-// Exemplos:
-// filtrarNumeros(["Javascript", 1, "3", "Web", 20]) // retornará [1, 20]
-// filtrarNumeros(["a", "c"]) // retornará []
-/* function filtrarNumeros(arr){
-    return arr.filter(el => typeof el === "number");
-}
-console.log(filtrarNumeros(["Javascript", 1, "3", "Web", 20]));
-console.log(filtrarNumeros(["a", "c"])); */
+// function filtrarNumeros(arr){
+//     return arr.filter(el => typeof el === "number");
+// }
+// console.log(filtrarNumeros(["Javascript", 1, "3", "Web", 20])); // retornará [1, 20]
+// console.log(filtrarNumeros(["a", "c"])); // retornará []
 
 
 
 /*** Pág 07 ***/
 
 // Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, formado pelos pares chave/valor que corresponde a um atributo do objeto. 
-// Exemplos:
-// objetoParaArray({   nome: "Maria", profissao: "Desenvolvedora de software"}) // irá retornar [["nome", "Maria"], ["profissao", "Desenvolvedora de Software"]]
-// objetoParaArray({   codigo: 11111, preco: 12000}) // irá retornar [["codigo", 11111], ["preco", 12000]]
-/* // function objetoParaArray(objeto){
+// function objetoParaArray(objeto){
 //     let arr = [];
 //     for (let chave in objeto){ arr.push(chave, objeto[chave]); }
 //     return arr;
@@ -268,21 +204,17 @@ console.log(filtrarNumeros(["a", "c"])); */
 // 1 solução, fazer com for, e apartir da chave pegar o valor
 // 2 solução, extrair as chaves, e apartir das chaves pegar o valor
 // 3 solução, extrair tudo de uma vez
-console.log(objetoParaArray({ nome: "Maria", profissao: "Desenvolvedora de software" }));
-console.log(objetoParaArray({ codigo: 11111, preco: 12000 })); */
-
+// console.log(objetoParaArray({ nome: "Maria", profissao: "Desenvolvedora de software" })); // irá retornar [["nome", "Maria"], ["profissao", "Desenvolvedora de Software"]]
+// console.log(objetoParaArray({ codigo: 11111, preco: 12000 })); // irá retornar [["codigo", 11111], ["preco", 12000]]
 
 
 // Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são pares e que também tenham índices pares.
 // Lembre-se que um número é par porque é divisível por 2, ou seja, o resto da divisão da divisão dele por 2 é zero.
-// Exemplos:
-// receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]) // retornará []
-// receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]) // retornará [10, 22]
-/* function receberSomenteOsParesDeIndicesPares(obj){
-    return obj.filter((value, index) => value % 2 === 0 && index % 2 === 0);
-}
-console.log(receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]));
-console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43])); */
+// function receberSomenteOsParesDeIndicesPares(obj){
+//     return obj.filter((value, index) => value % 2 === 0 && index % 2 === 0);
+// }
+// console.log(receberSomenteOsParesDeIndicesPares([1, 2, 3, 4])); // retornará []
+// console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43])); // retornará [10, 22]
 
 
 
@@ -398,59 +330,38 @@ console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43])); */
 
 /*** Pág 12 ***/
 
-// Desenvolva uma função que recebe um caractere e uma string como parâmetros e retorne a quantidade de vezes
-// que o caractere se repete na string. A função deverá ser case-sensitive, ou seja, irá diferenciar maiúsculas de
-// minúsculas.
-// Exemplos:
-// contarCaractere("r", "A sorte favorece os audazes") // retornará 2
-// contarCaractere("c", "Conhece-te a ti mesmo") // retornará 1
-// Resolução 1
-// function contarCaractere(caractereBuscado, frase) {
-// let contador = 0
-// for (let i = 0; i < frase.length; i++)
-// if (frase.charAt(i) === caractereBuscado)
-// contador++
-// return contador
+// Desenvolva uma função que recebe um caractere e uma string como parâmetros e retorne a quantidade de vezes que o caractere se repete na string. 
+// A função deverá ser case-sensitive
+// function contarCaractere(char, str){
+// //    return str.match(new RegExp(char, "g")).length ;
+
+//    return [...str].filter(caractere => caractere === char).length;
 // }
-// Resolução 2
-// function contarCaractere(caractereBuscado, frase) {
-// return [...frase].filter(caractere => caractere === caractereBuscado).length
-// }
-// A fim de criar um mecanismo de busca para sua aplicação, você precisa iniciar criando uma função para
-// identificar palavras semelhantes.
+// console.log(contarCaractere("r", "A sorte favorece os audazes")); // retornará 2
+// console.log(contarCaractere("c", "Conhece-te a ti mesmo")); // retornará 1
+
+
 // Escreva uma função que recebe como primeiro parâmetro uma palavra e, como segundo parâmetro, um array de
 // strings. A função deverá filtrar as palavras do array que contêm nelas a string do primeiro parâmetro.
-// Exemplos:
-// buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]) // retornará ["programação", "profissional"]
-// buscarPalavrasSemelhantes("java", ["javascript", "java", "c++"]) // retornará ["javascript", "java"]
-// Resolução 1
-// function buscarPalavrasSemelhantes(inicio, palavras) {
-// const resultado = []
-// for (let palavra of palavras)
-// if (palavra.includes(inicio))
-// resultado.push(palavra)
-// return resultado
+// function buscarPalavrasSemelhantes(word, phrase) {
+//     return phrase.filter(value => value.indexOf(word) !== -1);
+//     return phrase.filter(value => value.includes(word));    
 // }
-// Resolução 2
-// function buscarPalavrasSemelhantes(inicio, palavras) {
-// return palavras.filter(palavra => palavra.includes(inicio))
+// console.log(buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"])); // retornará ["programação", "profissional"]
+// console.log(buscarPalavrasSemelhantes("java", ["javascript", "java", "c++"])); // retornará ["javascript", "java"]
+
+
+// Desenvolva uma função que receba uma frase como parâmetro e retorne essa string somente com as consoantes, ou seja, sem as vogais.
+// function removerVogais(word){
+//     const vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+//     vowels.forEach( value => word = word.replace(value,""));
+//     return word;
 // }
-// Desenvolva uma função que receba uma frase como parâmetro e retorne essa string somente com as consoantes,
-// ou seja, sem as vogais.
-// Exemplos:
-// Resoluções - Exercícios - Curso Fundamentos de Programação 13
-// removerVogais("Cod3r") // retornará "Cd3r"
-// removerVogais("Fundamentos") // retornará "Fndmnts"
-// Resolução 1
-// function removerVogais(frase) {
-// const vogais = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
-// vogais.forEach( vogal => frase = frase.replace(vogal, '') )
-// return frase
+// function removerVogais(word){
+//     return word.replace(/[aeiou]/ig, '');
 // }
-// Resolução 2
-// function removerVogais(frase) {
-// return frase.replace(/[aeiou]/ig, '')
-// }
+// console.log(removerVogais("Cod3r")); // retornará "Cd3r"
+// console.log(removerVogais("Fundamentos")); // retornará "Fndmnts"
 
 
 
